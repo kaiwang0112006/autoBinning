26353bd542d89c0fcedf9cd2f67ec8b59ea4d9c5
kaiwang <wangkai17@wejoydata.com>
2019-12-04 15:15:43 +0800

    v1

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autoBinning",
+    version="0.1.0",
+    author="Kai Wang",
+    author_email="wangkai0112006@163.com",
+    description="A small package for feature autoBinning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kaiwang0112006/autoBinning",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
