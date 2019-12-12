acdc3868691318bf3c91bf3a0665beccef5a14aa
kaiwang <wangkai17@wejoydata.com>
2019-12-12 16:01:30 +0800

    0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="autoBinning",
-    version="0.1.1",
+    version="0.1.2",
     author="Kai Wang",
     author_email="wangkai0112006@163.com",
     description="A small package for feature autoBinning",
